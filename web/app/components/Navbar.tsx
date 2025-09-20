638e95cfcd164f1fc57f3e79de27f7190d58b9cb
blindenbaum <bl77@rice.edu>
2025-09-20 16:22:40 -0500

    header name change

--- web/app/components/Navbar.tsx
+++ web/app/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar(): React.ReactElement {
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
-                        <a href="/" className="text-2xl font-bold text-gray-800">Lingo<span className="text-blue-600">Games</span></a>
+                        <a href="/" className="text-2xl font-bold text-gray-800"><span className="text-blue-600">Adapt</span></a>
                     </div>
                     <div className="flex items-center space-x-2">
                         <a 
